2e27a21ce0974217cbbb95ef0ae58bbaf5382121
Di-Werman <118116167+Di-Werman@users.noreply.github.com>
2023-02-19 17:02:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+sayHelloTo('Игорь')
+
+function sayHelloTo (name) {
+    console.log ('Привет, ' + name)
+}
+
+// var sayHelloTo = function (name) {
+//      console.log ('Привет, ' + name)
+//  }
+//  sayHelloTo('Игорь')
